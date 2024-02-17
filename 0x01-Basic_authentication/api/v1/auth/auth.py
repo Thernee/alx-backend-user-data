72c705000ae4eff163286c5b40d31ce3fb3d8dad
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-17 17:59:39 +0100

    Fixed modeule import error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
-"""Module for authentication"""
-from typing import TypeVar
+"""
+Module for authentication
+"""
+from typing import TypeVar, List
 from flask import request
+User = TypeVar('User')
 
 
 class Auth:
-    """Manage API authentication"""
-    def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
+    """
+    Manage API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Handle require auth"""
         return False
 
     def authorization_header(self, request=None) -> str:
-        """Handle auth header"""
+        """
+        Handle auth header
+        """
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """Handle user on current session"""
+    def current_user(self, request=None) -> User:
+        """
+        Handle user on current session
+        """
         return None
