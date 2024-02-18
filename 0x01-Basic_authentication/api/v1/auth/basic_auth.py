4b55bc986ed1d5063d4347d470a9ff0a85cd68a1
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-18 20:46:29 +0100

    Implemented BasicAuth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Module for BasicAuth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class
+    """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
