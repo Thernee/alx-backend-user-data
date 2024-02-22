5638812acab9299f40cdf727dd5bf0d1ddf17724
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-22 15:29:42 +0100

    Fix: tryCatch for delete user session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,6 +86,9 @@ class Auth:
         """
         Updates current user session to None
         """
-        user = self._db.find_user_by(user_id=user_id)
-        self._db.update_user(user.id, session_id=None)
-        return None
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user.id, session_id=None)
+            return None
+        except Exception:
+            return None
