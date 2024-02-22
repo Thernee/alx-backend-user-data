c09de4b04be99a769f0c70d62a3aa0447b27ba62
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-22 15:16:31 +0100

    Implement: get_user_from_session_id()

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,15 @@ class Auth:
             return new_uuid
         except Exception:
             pass
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Get user based on session id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
+        return user
