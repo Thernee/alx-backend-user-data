7418b5ac6fb748368c357d1b8400cb4295683332
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-22 16:56:07 +0100

    Implement: update_password()

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -101,3 +101,15 @@ class Auth:
         new_reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=new_reset_token)
         return new_reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update user password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except Exception:
+            raise ValueError
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed_password,
+                             reset_token=None)
