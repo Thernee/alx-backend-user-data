6d7b632f78cd9dcadb90b554c5e38afc34860358
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-22 16:33:04 +0100

    Implement: uth.get_reset_password_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,3 +89,15 @@ class Auth:
         user = self._db.find_user_by(id=user_id)
         self._db.update_user(user.id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generate reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError
+        new_reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=new_reset_token)
+        return new_reset_token
