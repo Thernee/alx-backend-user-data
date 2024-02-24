3e24a38cb5265859ed4ff51ccd2a51270cc3979b
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-24 19:32:53 +0100

    Fix: session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,14 +11,14 @@ class SessionAuth(Auth):
     Session auth class
     """
 
-    def create_session(self, user_id: str = None) -> str:
-        """
-        Create session
-        """
-        if user_id is None:
-            return None
-        if type(user_id) is not str:
-            return None
-        session_id = self._generate_uuid()
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
+    # def create_session(self, user_id: str = None) -> str:
+    #     """
+    #     Create session
+    #     """
+    #     if user_id is None:
+    #         return None
+    #     if type(user_id) is not str:
+    #         return None
+    #     session_id = self._generate_uuid()
+    #     self.user_id_by_session_id[session_id] = user_id
+    #     return session_id
