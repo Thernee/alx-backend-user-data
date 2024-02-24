eec4f8959dc8adadd1cbed4f8c17fbcb6ad106bb
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-24 20:56:30 +0100

    Implement: create_session()

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 Module for session auth
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -10,18 +11,14 @@ class SessionAuth(Auth):
     """
     Session auth class
     """
-    pass
-    # validate if everything inherits correctly without any overloading
-    # validate the “switch” by using environment variables
+    user_id_by_session_id = {}
 
-    # def create_session(self, user_id: str = None) -> str:
-    #     """
-    #     Create session
-    #     """
-    #     if user_id is None:
-    #         return None
-    #     if type(user_id) is not str:
-    #         return None
-    #     session_id = self._generate_uuid()
-    #     self.user_id_by_session_id[session_id] = user_id
-    #     return session_id
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create new session id
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
