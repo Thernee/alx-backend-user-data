36ccaf7de1307827c6441f4aaa869867d46357d5
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-24 18:20:40 +0100

    Added: SessionAuth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Module for session auth
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session auth class
+    """
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create session
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = self._generate_uuid()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
