a1c77a23b02bc9ac776e7e13d2db40bd5344c7cd
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-26 11:20:54 +0100

    Implement: user login functionality

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+""" 
+Module of SessionAuth
+"""
+from flask import jsonify, request, Flask
+from api.v1.views.users import User
+from os import getenv
+
+app = Flask(__name__)
+
+
+@app.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ 
+    POST /api/v1/auth_session/login
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    elif password is None or email == '':
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({"email": email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not User.is_valid_password(user, password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    session_name = getenv('SESSION_NAME')
+    response.set_cookie(session_name, session_id)
+    return response
