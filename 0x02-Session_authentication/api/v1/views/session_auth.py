bc1461f23bf7d049862273e952ff0de5fcf98361
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-26 11:51:58 +0100

    fix: returned user object filter

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" 
+"""
 Module of SessionAuth
 """
 from flask import jsonify, request, Flask
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
-    """ 
+    """
     POST /api/v1/auth_session/login
     """
     email = request.form.get('email')
@@ -24,14 +24,15 @@ def login() -> str:
     elif password is None or email == '':
         return jsonify({"error": "password missing"}), 400
     user = User.search({"email": email})
-    if not user:
+    if not user[0]:
         return jsonify({"error": "no user found for this email"}), 404
-    if not User.is_valid_password(user, password):
+    if not user[0].is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
-    session_id = auth.create_session(user.id)
-    response = jsonify(user.to_json())
+    session_id = auth.create_session(user[0].id)
+    response = jsonify(user[0].to_json())
     session_name = getenv('SESSION_NAME')
     response.set_cookie(session_name, session_id)
+
     return response
