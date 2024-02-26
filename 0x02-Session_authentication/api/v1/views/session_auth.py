a74d69c12c232da96a03e662bbe3954b925f8782
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-26 12:05:08 +0100

    fix: list index out of range error

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -24,7 +24,7 @@ def login() -> str:
     elif password is None or email == '':
         return jsonify({"error": "password missing"}), 400
     user = User.search({"email": email})
-    if not user[0]:
+    if not user:
         return jsonify({"error": "no user found for this email"}), 404
     if not user[0].is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
